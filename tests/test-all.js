60566b22e17445dd284c32c54e24f0eb6697bb25
Irakli Gozalishvili <rfobic@gmail.com>
2011-02-09 04:59:23 +0100

    Changing code to follow jetpack code conventions.

--- tests/test-all.js
+++ tests/test-all.js
@@ -3,6 +3,5 @@
 exports["test traits from objects"] = require("./traits");
 exports["test traits from property descriptor maps"] = require("./descriptor");
 
-// Disabling this check since it is not yet supported by jetpack.
-//if (module == require.main)
-  require('test').run(exports)
+if (module == require.main)
+  require('test').run(exports);
